20ab99d7debb0e7b1897f9a29f88d6679696ae35
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-08 13:24:58 +0530

    added basic styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
+  rules: {
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+  },
   root: true,
   extends: '@react-native-community',
 };
---
+++ AppPro.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import {View, Text, StyleSheet, useColorScheme} from 'react-native';
+
+function AppPro(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.container}>
+      <Text style={isDarkMode ? styles.whiteText : styles.darkText}>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  whiteText: {
+    color: 'white',
+  },
+  darkText: {
+    color: 'black',
+  },
+});
+
+export default AppPro;
