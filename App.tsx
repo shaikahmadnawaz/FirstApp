7304c21c1cf3a6ebd0f00853e29c69580d49a687
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-06-08 12:35:57 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {View, Text, SafeAreaView} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World!</Text>
+        <Text>This is my first mobile app.</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
